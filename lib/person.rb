b67c100706c9a1ae518c698f81868e1ccde2c638
Joseph Kofler <koflerj93@gmail.com>
2020-07-15 16:06:12 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,19 +1,2 @@
-# your code goes here
-class Person
-
-     attr_reader :name, :happiness
-     attr_writer :bank_account
-    
-    def initialize(name)
-        @name = name
-        @happiness = 8
-    end
-    
-    def bank_account 
-       25
-    end 
-    
-    
-end
 
 
